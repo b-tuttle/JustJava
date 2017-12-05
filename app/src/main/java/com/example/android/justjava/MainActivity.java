dab92055c2adae924b22be975992f7a4244290da
Bryan Tuttle <b-tuttle@users.noreply.github.com>
2017-12-04 18:15:23 -0800

    Change quantity to be a global variable used in increment and decrement buttons

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -13,6 +13,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,24 +25,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
         display(quantity);
         displayPrice(quantity * 5);
     }
 
     /**
-     * This method is called when the increment button is clicked.
+     * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
-        int quantity = 3;
+        quantity = quantity + 1;
         display(quantity);
     }
 
     /**
-     * This method is called when the decrement button is clicked.
+     * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
-        int quantity = 1;
+        quantity = quantity - 1;
         display(quantity);
     }
 
