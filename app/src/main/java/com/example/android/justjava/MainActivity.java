3b6b7d69a585d6b1eb2dbe122d6be473e94f1877
Bryan Tuttle <b-tuttle@users.noreply.github.com>
2018-01-06 19:44:17 -0800

    Add field for user to input name and include it in the order summary.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -34,13 +35,14 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method creates an order summary
      *
+     * @param userName name input by user
      * @param price of the order
-     * @param hasWhippedCream bool indicating state of wc checkbox
+     * @param hasWhippedCream bool indicating state of whipped cream checkbox
      * @param hasChocolate bool indicating state of chocolate checkbox
      * @return a message with name, needed toppings, quantity ordered, total amount, and a thank you
      */
-    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate) {
-        String priceMessage = "Name: First Last";
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate, String userName) {
+        String priceMessage = "Name: " + userName;
         priceMessage += "\nAdd whipped cream? " + hasWhippedCream;
         priceMessage += "\nAdd chocolate? " + hasChocolate;
         priceMessage += "\nQuantity: " + quantity;
@@ -53,12 +55,18 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        // Take user's name and store as a string
+        EditText userName = findViewById(R.id.userName);
+        String nameText = userName.getText().toString();
+        // Find if user wants whipped cream
         CheckBox whippedCreamCheckBox = findViewById(R.id.whippedCreamCheckBox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        // Find if user wants chocolate
         CheckBox chocolateCheckBox = findViewById(R.id.chocolateCheckBox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
+
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate);
+        String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, nameText);
         displayMessage(priceMessage);
     }
 
